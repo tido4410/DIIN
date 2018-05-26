0e3d9b5efe9f90ed3028579a1bf46c6ce697e221
Gabriel Bronzatti Moro <gbmoro@inf.ufrgs.br>
2018-05-26 12:15:01 -0300

    Salary adapter

--- app/src/main/java/com/example/gabrielbronzattimoro/diiin/ui/SalaryListAdapter.kt
+++ app/src/main/java/com/example/gabrielbronzattimoro/diiin/ui/SalaryListAdapter.kt
@@ -1,24 +1,42 @@
 package com.example.gabrielbronzattimoro.diiin.ui
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.gabrielbronzattimoro.diiin.R
+import com.example.gabrielbronzattimoro.diiin.model.Salary
+import com.example.gabrielbronzattimoro.diiin.util.MathService
 
 
-class SalaryListAdapter : RecyclerView.Adapter<SalaryListAdapter.SalaryListItemViewHolder>() {
+class SalaryListAdapter(actxContext : Context, alstSalaryList: ArrayList<Salary>)  : RecyclerView.Adapter<SalaryListAdapter.SalaryListItemViewHolder>() {
+
+    private val mlstSalaryList: ArrayList<Salary> = alstSalaryList
+    private val mctxContext: Context = actxContext
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): SalaryListItemViewHolder? {
-        return null
+        return SalaryListItemViewHolder(LayoutInflater.from(parent?.context)
+                .inflate(R.layout.fragment_salarylist_item, parent, false))
     }
 
     override fun getItemCount(): Int {
-        return 0
+        return mlstSalaryList.size
     }
 
     override fun onBindViewHolder(holder: SalaryListItemViewHolder?, position: Int) {
+        val salaryItem = mlstSalaryList[position]
+
+        holder?.tvDate?.text = MathService.calendarTimeToString(salaryItem.mdtDate)
+        holder?.tvValue?.text = MathService.formatFloatToCurrency(salaryItem.msValue)
+        holder?.tvSource?.text = salaryItem.mstrSource
     }
 
 
-    class SalaryListItemViewHolder(avwView : ViewGroup) : RecyclerView.ViewHolder(avwView) {
-
+    class SalaryListItemViewHolder(avwView : View) : RecyclerView.ViewHolder(avwView) {
+        val tvValue: TextView = avwView.findViewById(R.id.tvValue)
+        val tvSource: TextView = avwView.findViewById(R.id.tvSource)
+        val tvDate: TextView = avwView.findViewById(R.id.tvDate)
     }
 }

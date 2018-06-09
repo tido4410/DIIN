6dcf24a0cd3e4eb56f31284d0222367875337a48
Gabriel Bronzatti Moro <gbmoro@inf.ufrgs.br>
2018-06-09 19:51:45 -0300

    Creating chart layout file

---
+++ app/src/main/java/diiin/ui/activity/ChartTesting.kt
@@ -0,0 +1,12 @@
+package diiin.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChartTesting : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chart_testing)
+    }
+}

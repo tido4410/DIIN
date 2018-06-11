d5ec05dc75e4f7e990f2d943b4368e0da184bb92
Gabriel Bronzatti Moro <gbmoro@inf.ufrgs.br>
2018-06-10 22:09:42 -0300

    Using canvas to draw chart pieces

--- app/src/main/java/diiin/ui/activity/ChartTesting.kt
+++ app/src/main/java/diiin/ui/activity/ChartTesting.kt
@@ -1,6 +1,5 @@
 package diiin.ui.activity
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -9,25 +8,89 @@ import android.graphics.RectF
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
+import br.com.gbmoro.diiin.R
+import diiin.model.Expense
+import diiin.model.ExpenseType
+import java.util.*
 
 class ChartTesting : AppCompatActivity() {
 
+    private var mllBaseContent : LinearLayout? = null
+    private var fpcFInancialPieChart : FinancialPieChart? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(PieChart(this))
+        setContentView(R.layout.activity_chart_testing)
+
+        mllBaseContent = findViewById(R.id.llBaseContent)
+
+        val expenseLst = ArrayList<Expense>()
+        expenseLst.add(Expense(0,120f, "", Date(), ExpenseType.TRANSPORT))
+        expenseLst.add(Expense(0,240f, "", Date(), ExpenseType.FOOD))
+        expenseLst.add(Expense(0,100f, "", Date(), ExpenseType.FUN))
+        expenseLst.add(Expense(0,145f, "", Date(), ExpenseType.OTHERS))
+        expenseLst.add(Expense(0,11f, "", Date(), ExpenseType.EDUCATION))
+        expenseLst.add(Expense(0,15f, "", Date(), ExpenseType.TRAVEL))
+        expenseLst.add(Expense(0,12f, "", Date(), ExpenseType.PETS))
+
+        fpcFInancialPieChart = FinancialPieChart(this, expenseLst)
+        mllBaseContent?.addView(fpcFInancialPieChart)
     }
 }
 
-class PieChart(actxContext : Context) : View(actxContext) {
+class FinancialPieChart(actxContext : Context, alstExpenseList : ArrayList<Expense>) : View(actxContext) {
+
+    private var mexpenseList : ArrayList<Expense> = alstExpenseList
+    private val mleftCoord : Float = 15f
+    private val mrightCoord : Float = 500 - mleftCoord
+    private val mtopCoord : Float = 20f
+    private val mbottomCoord : Float = 500 - mtopCoord
+    private val mBaseRect = RectF(mleftCoord, mtopCoord, mrightCoord, mbottomCoord)
+    private var msTotalValue : Float = 0f
+    private val msTotalDegree : Float = 360f
+    private val mpTempPaintObject : Paint = Paint()
 
     init {
         isFocusable = true
+
+        updateTotalValue()
     }
+
+    private fun updateTotalValue() {
+        msTotalValue = 0f
+        mexpenseList.forEach {
+            if(it.msValue!=null)
+                msTotalValue += it.msValue!!
+        }
+    }
+
+//    400 <-> 360
+//
+//    12 <-> x
+//
+//
+//    400x = 360 * 12
+//    x = 360 * 12 / 400
 
     override fun onDraw(canvas: Canvas) {
         //super.onDraw(canvas)
 
-        val pBlue = Paint()
+        var sCurrentPoint = 0f
+
+        mexpenseList.forEach {
+            if(it.msValue != null && it.metType != null) {
+                mpTempPaintObject.color = it.metType.backgroundColor(context)
+                val sSweepAngle = ((it.msValue!! * msTotalDegree) / msTotalDegree)
+                canvas.drawArc(mBaseRect, sCurrentPoint, sSweepAngle, true, mpTempPaintObject)
+                mpTempPaintObject.color = Color.BLACK
+                sCurrentPoint += sSweepAngle
+            }
+        }
+
+        canvas.drawRect(50f, mbottomCoord, 50f, mbottomCoord  + 200f, mpTempPaintObject)
+
+        /*val pBlue = Paint()
         pBlue.color = Color.BLUE
         val pRed = Paint()
         pRed.color = Color.RED
@@ -38,18 +101,10 @@ class PieChart(actxContext : Context) : View(actxContext) {
         val pYellow = Paint()
         pYellow.color = Color.YELLOW
 
-
-        val leftCoord = 15f
-        val rightCoord = resources.displayMetrics.widthPixels - leftCoord
-        val topCoord = 200f
-        val bottomCoord = resources.displayMetrics.heightPixels - topCoord
-
-        val rect = RectF(leftCoord, topCoord, rightCoord, bottomCoord)
-
-        canvas.drawArc(rect, 0f, 90f, true, pBlue)
-        canvas.drawArc(rect, 90f, 90f, true, pRed)
-        canvas.drawArc(rect, 180f, 90f, true, pGreen)
-        canvas.drawArc(rect, 270f, 90f, true, pYellow)
+        canvas.drawArc(mBaseRect, 0f, 90f, true, pBlue)
+        canvas.drawArc(mBaseRect, 90f, 90f, true, pRed)
+        canvas.drawArc(mBaseRect, 180f, 90f, true, pGreen)
+        canvas.drawArc(mBaseRect, 270f, 90f, true, pYellow)*/
     }
 
 }

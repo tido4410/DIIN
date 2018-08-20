65c883f19eeba79a6011bd42e2c2c7b599786515
gbmoro <tido4410@gmail.com>
2018-08-19 23:29:55 -0300

    First design to settings screen

---
+++ app/src/main/java/diiin/ui/activity/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package diiin.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.gbmoro.diiin.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
